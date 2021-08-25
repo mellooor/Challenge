f8698e0fa149a30076a234360e75542b97706829
Alex <rollemxela@gmail.com>
2021-08-25 19:34:45 +0100

    add initial parameter and methods to Parameters class

--- app/Classes/PDO/Parameters.php
+++ app/Classes/PDO/Parameters.php
@@ -6,5 +6,33 @@ namespace App\PDO;
 
 class Parameters
 {
+    private $dsn;
+    private $options;
+    private $user;
+    private $pass;
 
+    public function __construct($dsn, $user, $pass, $options = [])
+    {
+        $this->dsn = $dsn;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->options = $options;
+    }
+
+    public function getDsn()
+    {
+        return $this->dsn;
+    }
+    public function getOptions()
+    {
+        return $this->options;
+    }
+    public function getUser()
+    {
+        return $this->user;
+    }
+    public function getPass()
+    {
+        return $this->pass;
+    }
 }
