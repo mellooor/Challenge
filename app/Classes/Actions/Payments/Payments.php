621047f7a05717b4b3ef3df523a49efb85eaca6b
Alex <rollemxela@gmail.com>
2021-08-25 22:52:57 +0100

    reformat query in getPayments method

--- app/Classes/Actions/Payments/Payments.php
+++ app/Classes/Actions/Payments/Payments.php
@@ -61,21 +61,21 @@ class Payments extends DBAction
             ];
         }
 
-        $query = 'SELECT';
-        $query .= 'pl.plan_id, pc.pc_title, pc.pc_first_name, pc.pc_last_name, u.user_first_name, u.user_last_name, c.company_name';
-        $query .= 'FROM';
-        $query .= 'plan pl';
-        $query .= 'INNER JOIN plan_client pc ON pl.plan_id = pc.pc_plan_id';
-        $query .= 'INNER JOIN user u ON pl.plan_lead_gen = u.user_id';
-        $query .= 'INNER JOIN company c ON u.user_company = c.company_id';
-        $query .= 'INNER JOIN payment pay ON pl.plan_id = pay.payment_plan';
-        $query .= 'WHERE';
-        $query .= 'pay.payment_status = ?';
-        $query .= 'AND';
-        $query .= '(';
-        $query .= 'pay.payment_due_date > ?';
-        $query .= 'AND';
-        $query .= 'pay.payment_due_date < ?';
+        $query = 'SELECT ';
+        $query .= 'pl.plan_id, pc.pc_title, pc.pc_first_name, pc.pc_last_name, u.user_first_name, u.user_last_name, c.company_name ';
+        $query .= 'FROM ';
+        $query .= 'plan pl ';
+        $query .= 'INNER JOIN plan_client pc ON pl.plan_id = pc.pc_plan_id ';
+        $query .= 'INNER JOIN user u ON pl.plan_lead_gen = u.user_id ';
+        $query .= 'INNER JOIN company c ON u.user_company = c.company_id ';
+        $query .= 'INNER JOIN payment pay ON pl.plan_id = pay.payment_plan ';
+        $query .= 'WHERE ';
+        $query .= 'pay.payment_status = ? ';
+        $query .= 'AND ';
+        $query .= '( ';
+        $query .= 'pay.payment_due_date > ? ';
+        $query .= 'AND ';
+        $query .= 'pay.payment_due_date < ? ';
         $query .= ')';
 
         $pdo = $this->dbConnection->prepare($query);
